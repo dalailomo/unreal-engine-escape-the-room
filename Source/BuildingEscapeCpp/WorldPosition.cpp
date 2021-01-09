9755dee9f57e493ff49a1a971a48cc57e4b721f5
DalaiLomo <reisereise.85@gmail.com>
2021-01-09 13:44:59 +0100

    Logging out the name of the component's owner

--- Source/BuildingEscapeCpp/WorldPosition.cpp
+++ Source/BuildingEscapeCpp/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,10 +20,8 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Display, TEXT("This is a regular log"));
-	UE_LOG(LogTemp, Error, TEXT("This is an error"));
-	UE_LOG(LogTemp, Warning, TEXT("This is a warn"));
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("The object name is: %s"), *ObjectName);
 }
 
 
